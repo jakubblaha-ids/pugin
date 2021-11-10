c95e862eb642ccd67c37a7bc817ea60d180cff14
Jakub Bláha <jakub.blaha@gmail.com>
2021-11-10 16:02:44 +0100

    coming soon page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,102 +1,102 @@
 <script>
-    import Avatar from "$lib/components/Avatar.svelte";
-    import NavItem from "$lib/components/NavItem.svelte";
-    import TopBanner from "$lib/components/TopBanner.svelte";
-    import "../app.css";
+	import Avatar from '$lib/components/Avatar.svelte';
+	import NavItem from '$lib/components/NavItem.svelte';
+	import TopBanner from '$lib/components/TopBanner.svelte';
+	import '../app.css';
 
-    const navItems = [
-        {
-            title: "Timetable",
-            href: "/timetable",
-            icon: "/icons/calendar.svg",
-        },
-        {
-            title: "Courses",
-            href: "/courses",
-            icon: "/icons/academic-cap.svg",
-        },
-        {
-            title: "Lunch",
-            href: "/lunch",
-            icon: "/icons/cake.svg",
-        },
-        {
-            title: "Find person",
-            href: "/find-person",
-            icon: "/icons/search.svg",
-        },
-        {
-            title: "Settings",
-            href: "/settings",
-            icon: "/icons/cog.svg",
-        },
-    ];
+	const navItems = [
+		{
+			title: 'Timetable',
+			href: '/coming-soon',
+			icon: '/icons/calendar.svg'
+		},
+		{
+			title: 'Courses',
+			href: '/coming-soon',
+			icon: '/icons/academic-cap.svg'
+		},
+		{
+			title: 'Lunch',
+			href: '/coming-soon',
+			icon: '/icons/cake.svg'
+		},
+		{
+			title: 'Find person',
+			href: '/find-person',
+			icon: '/icons/search.svg'
+		},
+		{
+			title: 'Settings',
+			href: '/coming-soon',
+			icon: '/icons/cog.svg'
+		}
+	];
 
-    let navCollapsed = true;
-    $: navTranslateY = navCollapsed ? "80%" : "0%";
+	let navCollapsed = true;
+	$: navTranslateY = navCollapsed ? '80%' : '0%';
 
-    const toggleNav = () => (navCollapsed = !navCollapsed);
+	const toggleNav = () => (navCollapsed = !navCollapsed);
 
-    let innerHeight;
+	let innerHeight;
 </script>
 
 <svelte:window bind:innerHeight />
 
 <div class="grid place-items-center h-full">
-    <div
-        class="h-[800px] w-[400px]  rounded-2xl ring-2 ring-gray-100 flex flex-col bg-[#1E1E1E] overflow-hidden relative"
-        style="transform: scale({innerHeight < 800
-            ? `${innerHeight / 800}`
-            : '1'}); transform-origin: top center;"
-    >
-        <TopBanner />
+	<div
+		class="h-[800px] w-[400px]  rounded-2xl ring-2 ring-gray-100 flex flex-col bg-[#1E1E1E] overflow-hidden relative"
+		style="transform: scale({innerHeight < 800
+			? `${innerHeight / 800}`
+			: '1'}); transform-origin: top center;"
+	>
+		<TopBanner />
 
-        <div class="flex-grow overflow-y-scroll scrollbar-hide pb-20">
-            <slot />
-        </div>
+		<div class="flex-grow overflow-y-scroll scrollbar-hide pb-20">
+			<slot />
+		</div>
 
-        <!-- Navigation -->
-        <div
-            class="h-96 rounded-2xl bg-[#ECECEC] flex-shrink-0 shadow flex flex-col transition-transform duration-300 absolute bottom-0 w-full"
-            style="transform: translateY({navTranslateY});"
-        >
-            <div class="h-20 flex justify-around items-center flex-shrink-0">
-                <a class="h-10" href="/profile">
-                    <Avatar />
-                </a>
+		<!-- Navigation -->
+		<div
+			class="h-96 rounded-2xl bg-[#ECECEC] flex-shrink-0 shadow flex flex-col transition-transform duration-300 absolute bottom-0 w-full"
+			style="transform: translateY({navTranslateY});"
+		>
+			<div class="h-20 flex justify-around items-center flex-shrink-0">
+				<a class="h-10" href="/profile">
+					<Avatar />
+				</a>
 
-                <a href="/home">
-                    <svg
-                        class="w-8 h-8"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                        ><path
-                            d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
-                        /></svg
-                    >
-                </a>
+				<a href="/home">
+					<svg
+						class="w-8 h-8"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+						/></svg
+					>
+				</a>
 
-                <button on:click={toggleNav}>
-                    <svg
-                        class="w-8 h-8"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                        ><path
-                            fill-rule="evenodd"
-                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                            clip-rule="evenodd"
-                        /></svg
-                    >
-                </button>
-            </div>
+				<button on:click={toggleNav}>
+					<svg
+						class="w-8 h-8"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+							clip-rule="evenodd"
+						/></svg
+					>
+				</button>
+			</div>
 
-            <div class="overflow-y-scroll scrollbar-hide px-6 space-y-3 pb-6">
-                {#each navItems as item}
-                    <NavItem text={item.title} href={item.href} icon={item.icon} />
-                {/each}
-            </div>
-        </div>
-    </div>
+			<div class="overflow-y-scroll scrollbar-hide px-6 space-y-3 pb-6">
+				{#each navItems as item}
+					<NavItem text={item.title} href={item.href} icon={item.icon} />
+				{/each}
+			</div>
+		</div>
+	</div>
 </div>
---
+++ src/routes/coming-soon.svelte
@@ -0,0 +1,1 @@
+<div class="text-2xl font-mono text-white h-48 grid place-items-center">Coming soon...</div>
