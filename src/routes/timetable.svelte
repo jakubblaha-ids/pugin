f55b112bceb2bd460bafc145e6895e443eeaaa05
tomashobzaids <thomashobza@ids.company>
2021-11-10 13:14:20 +0100

    add: timetable

---
+++ src/routes/timetable.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import DayCard from "$lib/components/timetable/DayCard.svelte";
+
+    import { rozvrh } from "$lib/ts/timetable";
+
+    const dny = ["pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota", "neděle"];
+</script>
+
+<div class="flex flex-col w-full gap-4 px-4 font-mono py-4">
+    {#each rozvrh as day, i}
+        <div class="capitalize text-white font-3xl">{dny[i]}</div>
+        <DayCard predmety={day} />
+    {/each}
+</div>
