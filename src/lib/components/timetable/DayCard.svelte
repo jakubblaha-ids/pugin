6b93ce9118b90c47e9945214a28ffa5a06a03d1d
tomashobzaids <thomashobza@ids.company>
2021-11-10 13:14:20 +0100

    add: timetable

---
+++ src/lib/components/timetable/DayCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import Avatar from "../Avatar.svelte";
+
+    export let predmety: any[];
+    $: console.log(predmety);
+</script>
+
+<div class="bg-white rounded-xl px-2 py-3 flex flex-col gap-2">
+    {#each predmety as predmet}
+        <div class="bg-[#C4C4C4] p-4 rounded-xl flex flex-row items-center">
+            <div class="font-bold text-sm w-24">{predmet?.time || "00:00"}</div>
+            <div class="flex-1 px-2">{predmet?.name || "No name"}</div>
+            <div class="font-bold">{predmet?.room || "Room"}</div>
+        </div>
+    {/each}
+</div>
---
+++ src/lib/ts/timetable.ts
@@ -0,0 +1,7 @@
+export const rozvrh: any = Array(10).fill([
+    { time: "8:00-8:45", name: "Český jazyk", room: "211" },
+    { time: "8:55-9:40", name: "Dějepis", room: "420" },
+    { time: "9:50-10:35", name: "Matematika", room: "069" },
+    { time: "10:40-11:25", name: "Hudební výchova", room: "123" },
+    { time: "11:30-12:15", name: "Programování", room: "666" },
+]);
