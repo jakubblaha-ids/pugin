dd18d133a7b332d6b770b93311f91798eb85ddaf
Jakub Bláha <jakub.blaha@gmail.com>
2021-11-10 11:00:18 +0100

    timetable card

--- src/lib/components/Dashboard.svelte
+++ src/lib/components/Dashboard.svelte
@@ -1,6 +1,7 @@
 <script>
 	import DashboardCard from './DashboardCard.svelte';
 	import FeedbackCard from './FeedbackCard.svelte';
+	import TimetableCard from './TimetableCard.svelte';
 </script>
 
 <div class="flex flex-col space-y-4">
@@ -8,5 +9,9 @@
 
 	<DashboardCard>
 		<FeedbackCard />
+	</DashboardCard>
+
+	<DashboardCard>
+		<TimetableCard />
 	</DashboardCard>
 </div>
---
+++ src/lib/components/TimetableCard.svelte
@@ -0,0 +1,21 @@
+<script>
+	const lessons = ['D', 'M', 'Čj', 'Zsv', 'B', 'Tv'];
+</script>
+
+<div class="font-semibold text-lg">Today's lessons</div>
+
+<div class="flex space-x-2 mt-2 relative">
+	{#each lessons as lesson}
+		<div
+			class="rounded-xl bg-[#C4C4C4] w-12 h-12 grid place-items-center font-semibold first:opacity-50"
+		>
+			{lesson}
+		</div>
+	{/each}
+
+	<div class="bg-[#A1B7E2] w-1 h-16 rounded-full -top-2 left-10 absolute" />
+</div>
+
+<div class="font-semibold mt-4">
+	The next lesson starts in <span class="text-[#5783D9]">4 minutes</span>.
+</div>
