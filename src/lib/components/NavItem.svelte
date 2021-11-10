89e39b68109bb23a6dd308e6d25ec281bdee55a5
Jakub Bláha <jakub.blaha@gmail.com>
2021-11-10 10:43:55 +0100

    togglable navigation

---
+++ src/lib/components/NavItem.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let icon = '';
+	export let text = 'No Text';
+</script>
+
+<div class="rounded-2xl bg-[#C4C4C4] font-mono px-6 py-6 font-semibold relative flex">
+	<img src={icon} alt="" />
+
+	<div class="flex-grow">
+		{@html text}
+	</div>
+
+	<slot />
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,43 @@
 <script>
 	import Avatar from '$lib/components/Avatar.svelte';
+	import NavItem from '$lib/components/NavItem.svelte';
 	import TopBanner from '$lib/components/TopBanner.svelte';
 	import '../app.css';
+
+	const navItems = [
+		{
+			title: 'Timetable',
+			href: '/timetable',
+			icon: ''
+		},
+		{
+			title: 'Courses',
+			href: '/courses'
+		},
+		{
+			title: 'Lunch',
+			href: '/lunch'
+		},
+		{
+			title: 'Find person',
+			href: '/find-person'
+		},
+		{
+			title: 'Settings',
+			href: '/settings'
+		}
+	];
+
+	let navCollapsed = true;
+	$: navTranslateY = navCollapsed ? '80%' : '0%';
+
+	const toggleNav = () => (navCollapsed = !navCollapsed);
 </script>
 
 <div class="grid place-items-center h-screen">
-	<div class="h-[800px] w-[400px]  rounded-2xl ring-2 ring-gray-100 flex flex-col bg-[#1E1E1E]">
+	<div
+		class="h-[800px] w-[400px]  rounded-2xl ring-2 ring-gray-100 flex flex-col bg-[#1E1E1E] overflow-hidden"
+	>
 		<TopBanner />
 
 		<div class="flex-grow overflow-y-scroll scrollbar-hide">
@@ -14,36 +46,45 @@
 
 		<!-- Navigation -->
 		<div
-			class="h-20 flex justify-around items-center rounded-2xl bg-[#ECECEC] flex-shrink-0 shadow"
+			class="h-96 rounded-2xl bg-[#ECECEC] flex-shrink-0 shadow flex flex-col transition-transform duration-300"
+			style="transform: translateY({navTranslateY});"
 		>
-			<a class="h-10" href="/profile">
-				<Avatar />
-			</a>
+			<div class="h-20 flex justify-around items-center flex-shrink-0">
+				<a class="h-10" href="/profile">
+					<Avatar />
+				</a>
 
-			<a href="/home">
-				<svg
-					class="w-8 h-8"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
-					/></svg
-				>
-			</a>
+				<a href="/home">
+					<svg
+						class="w-8 h-8"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+						/></svg
+					>
+				</a>
 
-			<div>
-				<svg
-					class="w-8 h-8"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-						clip-rule="evenodd"
-					/></svg
-				>
+				<button on:click={toggleNav}>
+					<svg
+						class="w-8 h-8"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+							clip-rule="evenodd"
+						/></svg
+					>
+				</button>
+			</div>
+
+			<div class="overflow-y-scroll scrollbar-hide px-6 space-y-3 pb-6">
+				{#each navItems as item}
+					<NavItem text={item.title} />
+				{/each}
 			</div>
 		</div>
 	</div>
